38402a64b355d195ac197e0233a6a635be86e110
Adam Leskis <leskis@gmail.com>
2026-06-10 21:57:16 +0100

    Bump rootfs image version

--- scripts/lib/versions.sh
+++ scripts/lib/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 readonly ROOTFS_IMAGE_REPO="ghcr.io/lpmi-13/use-practice-rootfs"
-readonly DEFAULT_IXIMIUZ_ROOTFS_IMAGE="ghcr.io/lpmi-13/use-practice-rootfs:v2"
+readonly DEFAULT_IXIMIUZ_ROOTFS_IMAGE="ghcr.io/lpmi-13/use-practice-rootfs:v3"
 readonly DEFAULT_FIRST_PARTY_IMAGE_TAG="v1"
 readonly USE_TOOL_RELEASE_REPO="lpmi-13/use-tool"
